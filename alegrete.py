282d0a49719b4ecd14b15b9e0855eb21447b2816
gustavo auad piccoli <gustavo.a.piccoli@gmail.com>
2022-08-28 15:23:57 -0300

    algrete feito

--- alegrete.py
+++ alegrete.py
@@ -9,7 +9,12 @@ def compute_mse(theta_0, theta_1, data):
     :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
     :return: float - o erro quadratico medio
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    somatorio = 0
+    for d in data:
+        h = theta_1*d[0] + theta_0
+        somatorio += ((h-d[1])**2)
+    erro = somatorio/len(data)
+    return erro
 
 
 def step_gradient(theta_0, theta_1, data, alpha):
@@ -21,7 +26,19 @@ def step_gradient(theta_0, theta_1, data, alpha):
     :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
     :return: float,float - os novos valores de theta_0 e theta_1, respectivamente
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    somatorio0 = 0
+    for d in data:
+        h = theta_1*d[0] + theta_0
+        somatorio0 += ((h - d[1]))
+    derivada0 = (somatorio0 * 2) / len(data)
+    novo_theta_0 = theta_0 - (alpha*derivada0)
+    somatorio1 = 0
+    for d in data:
+        h = theta_1*d[0] + theta_0
+        somatorio1 += (h - d[1]) * d[0]
+    derivada1 = (somatorio1 * 2) / len(data)
+    novo_theta_1 = theta_1 - (alpha*derivada1)
+    return novo_theta_0, novo_theta_1
 
 
 def fit(data, theta_0, theta_1, alpha, num_iterations):
@@ -31,7 +48,6 @@ def fit(data, theta_0, theta_1, alpha, num_iterations):
     Ao final, retorna duas listas, uma com os theta_0 e outra com os theta_1
     obtidos ao longo da execução (o último valor das listas deve
     corresponder à última época/iteração).
-
     :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
     :param theta_0: float - intercepto da reta
     :param theta_1: float -inclinacao da reta
@@ -39,4 +55,10 @@ def fit(data, theta_0, theta_1, alpha, num_iterations):
     :param num_iterations: int - numero de épocas/iterações para executar a descida de gradiente
     :return: list,list - uma lista com os theta_0 e outra com os theta_1 obtidos ao longo da execução
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    theta_0_list = []
+    theta_1_list = []
+    for i in range(num_iterations):
+        theta_0, theta_1 = step_gradient(theta_0, theta_1, data, alpha)
+        theta_0_list.append(theta_0)
+        theta_1_list.append(theta_1)
+    return theta_0_list, theta_1_list
